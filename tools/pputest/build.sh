a13c8f4092fb1f45e682f25e1aa4785881a2b41c
𝑻𝒉𝒐𝒎𝒂𝒔 ~ パン <thomas462495@gmail.com>
2026-09-24 23:02:16 -0300

    test(spc700): build host regression test

--- tools/pputest/build.sh
+++ tools/pputest/build.sh
@@ -97,6 +97,15 @@ ROOT=../..
 
 "${CXX:-g++}" -O2 -ffunction-sections -fdata-sections \
     -Wl,--gc-sections \
+    -DCODE_PLATFORM=1 -DCODE_DEBUG=0 -DCODE_PROFILE=0 -DSNDBG_LOG=0 \
+    -I "$ROOT/src/common/base" \
+    -I "$ROOT/src/common/debug" \
+    -I "$ROOT/src/snes/apu" \
+    -I "$ROOT/src/snes/core" \
+    spc700_test.cpp "$ROOT/src/snes/apu/snspc_c.c" -o spc700_test
+
+"${CXX:-g++}" -O2 -ffunction-sections -fdata-sections \
+    -Wl,--gc-sections \
     -DCODE_PLATFORM=1 -DCODE_DEBUG=0 -DCODE_PROFILE=0 \
     -I "$ROOT/src/common/base" \
     -I "$ROOT/src/platform/ps2/system" \
@@ -110,4 +119,4 @@ ROOT=../..
     -I "$ROOT/src/snes/core" \
     diag_test.cpp -o diag_test
 
-echo "OK -> ./obj_test && ./oam_test && ./chrcache_test && ./hires_test && ./audioschedule_test && ./mode7_test && ./queue_test && ./spcio_test && ./safe_frameskip_test && ./diag_test"
+echo "OK -> ./obj_test && ./oam_test && ./chrcache_test && ./hires_test && ./audioschedule_test && ./mode7_test && ./queue_test && ./spcio_test && ./spc700_test && ./safe_frameskip_test && ./diag_test"
